a1cd08bc20587925b465c2ef652a4d2a110b6e68
Elzo Brito dos Santos Filho <elzo.santos@cps.sp.gov.br>
2021-10-29 17:41:13 -0300

    atualização

--- app/Controller/HomeController.php
+++ app/Controller/HomeController.php
@@ -2,9 +2,7 @@
 
 namespace OliviaApp\Controller;
 
-use OliviaApp\Models\Configuracoes;
 use OliviaLib\CommandController;
-
 use OliviaPublico\E404;
 use OliviaPublico\Login\Index;
 
@@ -13,9 +11,7 @@ class HomeController extends CommandController
 
     public function index()
     {
-        // $c = new Configuracoes();
-        // print_r($c->all());
-        //echo "<h1>It's Folks</h1>";
+        echo "<h1>It's Folks</h1>";
         // $parametros['nivel'] = '..';
         // new Index($parametros);
     }
