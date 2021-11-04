fe4c3b54eddc7eb1548e83457b93cc4023584e19
Elzo Brito dos Santos Filho <elzo.santos@cps.sp.gov.br>
2021-11-04 11:01:16 -0300

    Atualização

--- publico/E404.php
+++ publico/E404.php
@@ -21,7 +21,7 @@ class E404 extends ViewModelLogin
                         <div>
                             <img class="img-fluid w-75" src="./publico/assets/img/illustrations/404.svg" alt="404 not found">
                             <h1 class="mt-5">Página não <span class="font-weight-bolder text-primary">encontrada</span></h1>
-                            <p class="lead my-4">Oops! Parece que o a página que você está acessando não existe.</p>
+                            <p class="lead my-4">Oops! Parece que a página que você está tentando acessar não existe.</p>
                             <p class="lead my-4"><a href="./home">Voltar</a></p>
                         </div>
                     </div>
